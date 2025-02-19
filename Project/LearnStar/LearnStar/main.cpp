65559d221fca918a0598e7dc544d50785383cd74
Chensc <15759081770@qq.com>
2025-02-19 22:32:01 +0800

    first code modify

--- Project/LearnStar/LearnStar/main.cpp
+++ Project/LearnStar/LearnStar/main.cpp
@@ -3,11 +3,12 @@
 using namespace std;
 
 int main() {
-	int a = 0b1;
-	cout << (~a ) << endl;
 
-	int b = 0b11111111111111111111111111111110;
-	cout << (~b) << endl;
+	int a,b;
+	while(cin >> a >> b)
+	{
+		cout << a + b << endl;
+	}
 
 	return 0;
 }
